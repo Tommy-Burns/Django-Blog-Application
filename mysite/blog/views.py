1eb4bfe752aa9666a0aec7e7d932d3549d42c208
Tommy-Burns <58440453+Tommy-Burns@users.noreply.github.com>
2022-10-07 18:19:57 +0000

    up to sharing posts by email

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import get_object_or_404, render
+
+from .forms import EmailPostForm
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
+
+
 
 # Create your views here.
 def post_list(request):
@@ -30,3 +36,32 @@ def post_detail(request, year, month, day, post):
     return render(request, 'blog/post/detail.xhtml', {
         'post': post,
     })
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends that you read '{post.title}'"
+            message = f"Read {post.title} at {post_url}\n\n"\
+                      f"{cd['name']} wrote the following comment about the post:\n {cd['comments']}"
+            send_mail(subject, message, 'tbbotchwey01@gmail.com', [cd['to']])
+            
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.xhtml', {
+        'post': post,
+        'form': form,
+        'sent': sent,
+    })            
+class PostListView(ListView):
+    queryset: Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.xhtml'
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from env.details import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
